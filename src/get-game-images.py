8c8f14f21c847170caa13ef8242c0a592d018bee
diguifi <lumia-diguifi@hotmail.com>
2022-12-25 12:36:36 -0300

    Improve images getter script to optionaly be fed by a csv

--- src/get-game-images.py
+++ src/get-game-images.py
@@ -1,8 +1,24 @@
 import requests
 import json
+import sys
+import csv
 
+
+games = [{'name': 'Halo 5: Guardians', 'copies': '0.0m'}]
 YOUR_CLIENT_ID = ''
 YOUR_APP_ACCESS_TOKEN = ''
+
+
+def get_top_3(csv_file_name):
+    top_3_games = []
+    with open('../data/' + csv_file_name + '.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        top = 0
+        for row in spamreader:
+            if top <= 3 and top != 0:
+                top_3_games.append({'name': row[0], 'copies': row[1]})
+            top += 1
+    return top_3_games
 
 def log_on_api(access_token):
     base_url = 'https://id.twitch.tv/oauth2/token'
@@ -15,6 +31,7 @@ def log_on_api(access_token):
             json_text = json.loads(result.text)
             access_token = json_text["access_token"]
             return access_token
+    return access_token
 
 def get_game_cover_by_name(returned_token, game_name):
     raw_data = f'fields name, cover.url, cover.image_id; search "{game_name}"; limit 1;'
@@ -31,14 +48,17 @@ def get_game_cover_by_name(returned_token, game_name):
 
 def main():
     html = ''
-    games = ['Grand Theft Auto V', 'Wii Sports', 'PUBG: Battlegrounds', 'Super Mario Bros.']
     access_token = ''
     returned_token = log_on_api(access_token)
 
     for game in games:
-        cover_url = get_game_cover_by_name(returned_token, game)
-        html += f'<img src="{cover_url}" alt="{game}" width="264" height="352">'
+        cover_url = get_game_cover_by_name(returned_token, game["name"])
+        html += f'<div class="game-card">\n<img class="game-card-cover" src="{cover_url}" alt="{game["name"]}" height="176" width="132">\n<h5 class="game-card-title">{game["name"]}</h5>\n<p class="game-card-description">Copies sold: {game["copies"]}</p>\n</div>\n'
 
     print(html)
+
+
+if len(sys.argv) > 1:
+    games = get_top_3(sys.argv[1])
 
 main()
